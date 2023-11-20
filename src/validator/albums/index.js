303b842b48601bb10f04f984b345f8c40e6b64b9
kuma-coffee <daniel099d@gmail.com>
2023-11-20 15:52:29 +0700

    add validator

---
+++ src/validator/albums/index.js
@@ -0,0 +1,20 @@
+const { AlbumPayloadSchema, SongPayloadSchema } = require("./schema");
+const InvariantError = require("../../exceptions/InvariantError");
+
+const AlbumValidator = {
+  validateAlbumPayload: (payload) => {
+    const validationResult = AlbumPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+
+  validatSongPayload: (payload) => {
+    const validationResult = SongPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AlbumValidator;

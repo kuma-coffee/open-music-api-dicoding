8b6259f6ee903ba9497d197a522222e60025dbe3
kuma-coffee <daniel099d@gmail.com>
2023-11-21 14:44:44 +0700

    fix server and package

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+const openmusicapi = require("./api");
+const AlbumsService = require("./services/postgres/albumsService");
+const SongsService = require("./services/postgres/songsService");
+const Validator = require("./validator/index");
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
   const server = Hapi.Server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -13,10 +19,19 @@ const init = async () => {
     },
   });
 
-  await server.register({});
+  await server.register({
+    plugin: openmusicapi,
+    options: {
+      service: {
+        albumsService,
+        songsService,
+      },
+      validator: Validator,
+    },
+  });
 
   await server.start();
-  console.log(`Server running on ${server.infor.url}`);
+  console.log(`Server running on ${server.info.uri}`);
 };
 
 init();
